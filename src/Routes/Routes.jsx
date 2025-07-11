e4bf49e1208b2d8e1d09ed2ac8501610f1d7c9d8
samiul-alim12 <nawabitsamiul@gmail.com>
2025-07-11 19:30:46 +0600

    basic setup done for project

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className=" text-center text-4xl py-10">Hello World</div>,
+  },
+]);
